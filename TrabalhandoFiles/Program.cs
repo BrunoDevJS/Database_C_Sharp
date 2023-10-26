a4b0e65b0cee17f82e5f65a7adc051c5a4ac316a
BrunoDevJS <bruno.developerfullstack@gmail.com>
2023-10-26 20:08:31 -0300

    commit inicial

---
+++ TrabalhandoFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"C:\temp\dados_iniciais.txt";
+            string targetPath = @"C:\temp\dados_finais.txt";
+
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            using (StreamWriter sw = File.AppendText(targetPath)) {
+
+                for (int i = 1; i <= lines.Length - 1; i++) {
+                    sw.Write(lines[i]);
+                    sw.WriteLine($"; {i}; ");
+                }
+            }    
+        }
+    }
+}
---
+++ TrabalhandoFiles/README.md
@@ -0,0 +1,3 @@
+# Database_C_Sharp
+Banco de dados com adição do id ao final a partir do arquivo dados_iniciais.txt ao arquivo dados_finais.txt.
+Foi usado a partir da pasta temp do disco local C.
